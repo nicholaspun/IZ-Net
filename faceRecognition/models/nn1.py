2f30efbb906a34d30810691d8a6322f517e559e7
Nicholas Pun <nicholaspun99@gmail.com>
2020-08-30 17:32:42 -0600

    Fix NN1

--- faceRecognition/models/nn1.py
+++ faceRecognition/models/nn1.py
@@ -1,6 +1,6 @@
 from keras import backend as K
 from keras.models import Model
-from keras.layers import BatchNormalization, Conv2D, Dense, Flatten, Input, Lambda, MaxPool2D
+from keras.layers import BatchNormalization, Conv2D, Dense, Flatten, Input, Lambda, MaxPool2D, Maximum
 
 def base_network():
   X_input = Input((224, 224, 3))
@@ -8,7 +8,7 @@ def base_network():
   X = Conv2D(64, (7, 7), strides=(2, 2), activation='relu',
              padding='same', data_format='channels_last')(X_input)
   X = BatchNormalization()(X)
-  X = MaxPool2D(strides=(1, 1))(X)
+  X = MaxPool2D((3, 3), strides=(2, 2), padding='same')(X)
 
   X = Conv2D(64, (1, 1), activation='relu')(X)
   X = Conv2D(192, (3, 3), activation='relu', padding='same')(X)
@@ -27,19 +27,17 @@ def base_network():
   X = Conv2D(256, (1, 1), activation='relu')(X)
   X = Conv2D(256, (3, 3), activation='relu', padding='same')(X)
   X = BatchNormalization()(X)
-  X = MaxPool2D()(X)
+  X = MaxPool2D((3, 3), strides=(2, 2), padding='same')(X)
 
   X = Flatten()(X)
-  X = Dense(4096, activation='relu')(X)
-  X = Dense(4096, activation='relu')(X)
 
-  # X_d1 = Dense(4096)(X)
-  # X_d2 = Dense(4096)(X)
-  # X = Maximum()([X_d1, X_d2])
+  X_d1 = Dense(4096)(X)
+  X_d2 = Dense(4096)(X)
+  X = Maximum()([X_d1, X_d2])
 
-  # X_d1 = Dense(4096)(X)
-  # X_d2 = Dense(4096)(X)
-  # X = Maximum()([X_d1, X_d2])
+  X_d1 = Dense(4096)(X)
+  X_d2 = Dense(4096)(X)
+  X = Maximum()([X_d1, X_d2])
 
   X = Dense(128, activation='relu')(X)
   X = Lambda(lambda x: K.l2_normalize(x, axis=1))(X)
